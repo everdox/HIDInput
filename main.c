82b592548a18161ea2774be41fed1fa965616fdf
everdox <everdox@gmail.com>
2014-09-22 17:10:35 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+ /*
  This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -12,7 +13,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "input.h"
-
+*/
 
 //////////////////////////////////
 //                               //
